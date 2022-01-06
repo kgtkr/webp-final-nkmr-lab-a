17a1d0b2075e132515385bb96cd684b973acc062
kgtkr <contact@kgtkr.net>
2022-01-06 18:29:48 +0900

    seed dataでdeleted_atをnullに

--- sql/insert_seed.sql
+++ sql/insert_seed.sql
@@ -2,9 +2,11 @@
 .import 'seed/users.csv' users
 .import 'seed/tags.csv' tags
 UPDATE tags SET image_filename = NULL WHERE image_filename = '';
+UPDATE tags SET deleted_at = NULL WHERE deleted_at = '';
 .import 'seed/tag_incompatible_ralations.csv' tag_incompatible_ralations
 .import 'seed/clohtes.csv' clohtes
 UPDATE clohtes SET image_filename = NULL WHERE image_filename = '';
+UPDATE clohtes SET deleted_at = NULL WHERE deleted_at = '';
 .import 'seed/clothes_tags.csv' clothes_tags
 .import 'seed/laundries.csv' laundries
 .import 'seed/laundry_clothes.csv' laundry_clothes
