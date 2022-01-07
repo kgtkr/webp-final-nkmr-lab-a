f1fe83438dad0ebfc360c1fd279b270a79cc2686
kgtkr <contact@kgtkr.net>
2022-01-08 00:46:58 +0900

    inを使ってn+1問題を解決する例

--- lib/db.php
+++ lib/db.php
@@ -4,4 +4,22 @@ function connectDB() {
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 }
+
+function array_prepare_query($key, $n) {
+    $query = "(";
+    for ($i = 0; $i < $n; $i++) {
+        $query .= ":$key$i";
+        if ($i < $n - 1) {
+            $query .= ",";
+        }
+    }
+    $query .= ")";
+    return $query;
+}
+
+function array_prepare_bind($stat, $key, $array, $type) {
+    foreach ($array as $i => $value) {
+        $stat->bindValue(":$key$i", $value, $type);
+    }
+}
 ?>
