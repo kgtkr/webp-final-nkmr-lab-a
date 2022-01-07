078c58c7d6b7e47a52627f72d9d28109da385664
kgtkr <contact@kgtkr.net>
2022-01-08 00:01:17 +0900

    preludeファイルを作った

--- lib/db.php
+++ lib/db.php
@@ -1,7 +1,7 @@
 <?php
-    function connectDB() {
-        $db = new PDO("sqlite:" . dirname(__FILE__) . "/../db/db.sqlite");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $db;
-    }
+function connectDB() {
+    $db = new PDO("sqlite:" . dirname(__FILE__) . "/../db/db.sqlite");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+}
 ?>
