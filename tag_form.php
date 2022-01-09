b906fd330785978c12ad7535232561e0d2d52cda
kgtkr <contact@kgtkr.net>
2022-01-09 15:53:59 +0900

    tag編集ページで前回設定した画像を保持できるようにした
    細かいデザイン変更/image.phpにexist追加/画像ファイル名生成アルゴリズム変更

--- tag_form.php
+++ tag_form.php
@@ -8,26 +8,18 @@
 
     $action = $_POST['action'] ?? null;
 
-    $tag = null;
-    if ($id !== null) {
-        $stmt = $db->prepare('SELECT * FROM tags WHERE id = :id AND user_id = :user_id AND deleted_at IS NULL');
-        $stmt->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
-        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-        $tag = $stmt->fetch();
-        if ($tag === false) {
-            $tag = null;
-        }
-    }
-
     if ($action !== null && verify_csrf_token()) {
         if ($action === 'submit') {
             $name = $_POST['name'] ?? '';
             $image = $_FILES['image'] ?? null;
+            $prev_image_filename = $_POST['image_filename'] ?? null;
             if ($image !== null) {
                 $image_filename = image\save($image);
             } else {
                 $image_filename = null;
+            }
+            if ($image_filename === null && $prev_image_filename !== null && image\exist($prev_image_filename)) {
+                $image_filename = $prev_image_filename;
             }
 
             if ($id === null) {
@@ -40,6 +32,18 @@
             $stmt->bindValue(':name', $name, PDO::PARAM_STR);
             $stmt->bindValue(':image_filename', $image_filename, PDO::PARAM_STR);
             $stmt->execute();
+        }
+    }
+
+    $tag = null;
+    if ($id !== null) {
+        $stmt = $db->prepare('SELECT * FROM tags WHERE id = :id AND user_id = :user_id AND deleted_at IS NULL');
+        $stmt->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $tag = $stmt->fetch();
+        if ($tag === false) {
+            $tag = null;
         }
     }
 ?>
@@ -57,9 +61,26 @@
         <form action="tag_form.php<?php if ($id !== null) { echo '?id=' . $id; } ?>" method="post"  enctype="multipart/form-data">
             <?php echo_csrf_token(); ?>
             <input type="hidden" name="action" value="submit">
-            <input type="text" name="name" value="<?php if ($tag !== null) { echo h($tag['name']); } ?>">
-            <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-            <input type="file" name="image">
+            <div>
+                <label for="name">タグ名</label>
+                <input type="text" name="name" value="<?php if ($tag !== null) { echo h($tag['name']); } ?>">
+            </div>
+            <div>
+                <label for="image">画像</label>
+                <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+                <input type="file" name="image">
+            </div>
+            <?php if ($tag !== null && $tag['image_filename'] !== null) { ?>
+                <div>
+                    <div>
+                        <img src="images/<?php echo h($tag['image_filename']); ?>">
+                    </div>
+                    <div>
+                        <label for="image_filename">画像を保持</label>
+                        <input type="checkbox" name="image_filename" value="<?php echo h($tag['image_filename']); ?>" checked>
+                    </div>
+                </div>
+            <?php } ?>
             <input type="submit" value="送信">
     <?php } ?>
 </body>
