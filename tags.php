8f2dbec38369775212113999f6e0940aface72f3
kgtkr <contact@kgtkr.net>
2022-01-09 16:28:27 +0900

    tagの関係を定義する部分作成

--- tags.php
+++ tags.php
@@ -14,6 +14,76 @@
             $stmt->bindValue(':id', $id, PDO::PARAM_INT);
             $stmt->execute();
         }
+
+        if ($action === 'add_relation') {
+            $tag_id1 = intval($_POST['tag_id1'] ?? null);
+            $tag_id2 = intval($_POST['tag_id2'] ?? null);
+            if ($tag_id1 > $tag_id2) {
+                $tmp = $tag_id1;
+                $tag_id1 = $tag_id2;
+                $tag_id2 = $tmp;
+            }
+            $stat = $db->prepare('SELECT * FROM tags WHERE id = :tag_id1 AND user_id = :user_id AND deleted_at IS NULL');
+            $stat->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+            $stat->bindValue(':tag_id1', $tag_id1, PDO::PARAM_INT);
+            $stat->execute();
+            $tag1 = $stat->fetch(PDO::FETCH_ASSOC);
+            if ($tag1 === false) {
+                $tag1 = null;
+            }
+            $stat = $db->prepare('SELECT * FROM tags WHERE id = :tag_id2 AND user_id = :user_id AND deleted_at IS NULL');
+            $stat->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+            $stat->bindValue(':tag_id2', $tag_id2, PDO::PARAM_INT);
+            $stat->execute();
+            $tag2 = $stat->fetch(PDO::FETCH_ASSOC);
+            if ($tag2 === false) {
+                $tag2 = null;
+            }
+            if ($tag1 !== null && $tag2 !== null) {
+                $stat = $db->prepare('INSERT INTO tag_incompatible_ralations (tag_id1, tag_id2) VALUES (:tag_id1, :tag_id2)');
+                $stat->bindValue(':tag_id1', $tag_id1, PDO::PARAM_INT);
+                $stat->bindValue(':tag_id2', $tag_id2, PDO::PARAM_INT);
+                try {
+                    $stat->execute();
+                } catch (PDOException $e) {
+                    if ($e->getCode() !== '23000') {
+                        throw $e;
+                    }
+                }
+            }
+        }
+
+        if ($action === 'delete_relation') {
+            $tag_id1 = intval($_POST['tag_id1'] ?? null);
+            $tag_id2 = intval($_POST['tag_id2'] ?? null);
+            if ($tag_id1 > $tag_id2) {
+                $tmp = $tag_id1;
+                $tag_id1 = $tag_id2;
+                $tag_id2 = $tmp;
+            }
+            $stat = $db->prepare('SELECT * FROM tags WHERE id = :tag_id1 AND user_id = :user_id AND deleted_at IS NULL');
+            $stat->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+            $stat->bindValue(':tag_id1', $tag_id1, PDO::PARAM_INT);
+            $stat->execute();
+            $tag1 = $stat->fetch(PDO::FETCH_ASSOC);
+            if ($tag1 === false) {
+                $tag1 = null;
+            }
+            $stat = $db->prepare('SELECT * FROM tags WHERE id = :tag_id2 AND user_id = :user_id AND deleted_at IS NULL');
+            $stat->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+            $stat->bindValue(':tag_id2', $tag_id2, PDO::PARAM_INT);
+            $stat->execute();
+            $tag2 = $stat->fetch(PDO::FETCH_ASSOC);
+            if ($tag2 === false) {
+                $tag2 = null;
+            }
+            if ($tag1 !== null && $tag2 !== null) {
+                $stat = $db->prepare('DELETE FROM tag_incompatible_ralations WHERE tag_id1 = :tag_id1 AND tag_id2 = :tag_id2');
+                $stat->bindValue(':tag_id1', $tag_id1, PDO::PARAM_INT);
+                $stat->bindValue(':tag_id2', $tag_id2, PDO::PARAM_INT);
+                $stat->execute();
+            }
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -25,8 +95,25 @@
 <body>
     <h1>タグ一覧</h1>
     <?php if ($login_user_id === null) { ?>
-        <p>ログインしてください</p>
-    <?php } else { ?>
+        <div>ログインしてください</div>
+    <?php
+        } else { 
+        $stat = $db->prepare('SELECT * FROM tags WHERE user_id = :user_id AND deleted_at IS NULL ORDER BY id');
+        $stat->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+        $stat->execute();
+        $tags = $stat->fetchAll();
+
+        $tagHash = [];
+        foreach ($tags as $tag) {
+            $tagHash[$tag['id']] = $tag;
+        }
+
+        $stmt = $db->prepare('SELECT * FROM tag_incompatible_ralations WHERE tag_id1 IN ' . array_prepare_query('tag_id', count($tags)));
+        array_prepare_bind($stmt, 'tag_id', array_column($tags, 'id'), PDO::PARAM_INT);
+        $stmt->execute();
+        $relations = $stmt->fetchAll();
+    ?>
+        <div><a href="tag_form.php">新規作成</a></div>
         <table border="1">
             <tr>
                 <th>タグ名</th>
@@ -59,6 +146,48 @@
                     </td>
                 </tr>
             <?php } ?>
+        </table>
+        <h2>組み合わせてはいけないタグの設定</h2>
+        <form action="tags.php" method="post">
+            <input type="hidden" name="action" value="add_relation">
+            <?php echo_csrf_token(); ?>
+            <select name="tag_id1">
+                <?php foreach ($tags as $tag) { ?>
+                    <option value="<?php echo h($tag['id']); ?>"><?php echo h($tag['name']); ?></option>
+                <?php } ?>
+            </select>
+            <select name="tag_id2">
+                <?php foreach ($tags as $tag) { ?>
+                    <option value="<?php echo h($tag['id']); ?>"><?php echo h($tag['name']); ?></option>
+                <?php } ?>
+            </select>
+            <input type="submit" value="追加">
+        </form>
+        <table border='1'>
+            <tr>
+                <th>タグ1</th>
+                <th>タグ2</th>
+                <th>削除</th>
+            </tr>
+            <?php
+                foreach ($relations as $relation) {
+                ?>
+                <tr>
+                    <td><?php echo h($tagHash[$relation['tag_id1']]['name']); ?></td>
+                    <td><?php echo h($tagHash[$relation['tag_id2']]['name']); ?></td>
+                    <td>
+                        <form action="tags.php" method="post">
+                            <input type="hidden" name="tag_id1" value="<?php echo h($relation['tag_id1']); ?>">
+                            <input type="hidden" name="tag_id2" value="<?php echo h($relation['tag_id2']); ?>">
+                            <input type="hidden" name="action" value="delete_relation">
+                            <?php echo_csrf_token(); ?>
+                            <input type="submit" value="削除">
+                        </form>
+                    </td>
+                </tr>
+                <?php
+                }
+            ?>
         </table>
     <?php } ?>
 </body>
