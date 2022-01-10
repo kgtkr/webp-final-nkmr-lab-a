a48b35c9d3be3d3448a2767eb29daa9f80018c5f
kgtkr <contact@kgtkr.net>
2022-01-11 01:29:20 +0900

    グループ分けにタグを表示できるようにした

--- laundry.php
+++ laundry.php
@@ -31,20 +31,56 @@ $clothes_db=$db->prepare("SELECT * FROM clohtes WHERE id IN ".array_prepare_quer
 array_prepare_bind($clothes_db, 'clothes_id', $clothes_ids, PDO::PARAM_INT);
 $clothes_db->execute();
 $clothes=$clothes_db->fetchAll();
+$stat=$db->prepare("
+    SELECT
+        tags.id as id,
+        tags.name as name,
+        tags.image_filename as image_filename,
+        clothes_tags.clothes_id as clothes_id
+    FROM
+        clothes_tags JOIN tags ON clothes_tags.tag_id=tags.id
+    WHERE
+        tags.user_id=:user_id AND tags.deleted_at IS NULL AND clothes_tags.clothes_id IN ".array_prepare_query('clothes_id', count($clothes_ids))."");
+array_prepare_bind($stat, 'clothes_id', $clothes_ids, PDO::PARAM_INT);
+$stat->bindValue(':user_id', $login_user_id, PDO::PARAM_STR);
+$stat->execute();
+$tags=$stat->fetchAll();
+$tagHash=[];
+foreach($tags as $tag){
+    $tagHash[$tag['clothes_id']][]=$tag;
+}
 $groups = [];
+$group_tags = [];
 foreach($laundries as $laundry){
     $groups[$laundry['group_id']][] = $laundry['clothes_id'];
+    foreach($tagHash[$laundry['clothes_id']] ?? [] as $tag){
+        $group_tags[$laundry['group_id']][$tag['id']] = $tag;
+    }
 }
 $clothes_id_to_clothes = [];
 foreach($clothes as $clothe){
     $clothes_id_to_clothes[$clothe['id']] = $clothe;
 }
 foreach($groups as $group_id=>$clothe_ids){
-    echo '<ul class="group">';
+    echo '<div class="group">';
+    echo '<h2>タグ</h2>';
+    echo '<ul>';
+    foreach($group_tags[$group_id] ?? [] as $tag){
+        echo '<li>';
+        if ($tag['image_filename']) {
+            echo '<img src="images/'.$tag['image_filename'].'">';
+        }
+        echo h($tag['name']);
+        echo '</li>';
+    }
+    echo '</ul>';
+    echo '<h2>服</h2>';
+    echo '<ul>';
     foreach($clothe_ids as $clothe_id){
         echo '<li>' . h($clothes_id_to_clothes[$clothe_id]['name']) . "</li>";
     }
     echo '</ul>';
+    echo '</div>';
 }
 
 ?>
